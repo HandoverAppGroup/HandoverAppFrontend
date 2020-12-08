af1ce1e2a762dc17a9d17628e71caaa6d041e4d8
nrimsky <ninarimsky@gmail.com>
2020-12-08 17:19:44 +0000

    add npm dependencies to package.json and delete unused files

--- src/components/AddUser.js
+++ src/components/AddUser.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import axios from 'axios'
 import { Link, useHistory } from "react-router-dom";
-import{FormGroup} from "reactstrap"
 
 const AddUser = () => {
   let history = useHistory();
