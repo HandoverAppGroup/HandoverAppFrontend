ffccb06174bbf6616e823c09ae70d55ec4a240de
nrimsky <ninarimsky@gmail.com>
2020-12-13 18:24:17 +0000

    resolve merge conflicts

--- src/App.js
+++ src/App.js
@@ -10,39 +10,20 @@ import Nav from 'react-bootstrap/Nav';
 export default function App() {
   return (
     <BrowserRouter>
-<<<<<<< HEAD
-      <div>
-        <header>
-          <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="/"><b>Charing Cross Hospital Handover Portal</b></Navbar.Brand>
-            <Nav className="mr-auto">
-              <Nav.Link href="/"><b>All tasks</b></Nav.Link>
-              <Nav.Link href="/Archive"><b>Archive</b></Nav.Link>
-              <Nav.Link href="/tasks/add"><b>Add a task</b></Nav.Link>
-            </Nav>
-          </Navbar>
-        </header>
-        <Switch>
-          <Route exact path="/" component={TaskTable} />
-          <Route exact path="/Archive" component={Archive} />
-          <Route exact path="/tasks/add" component={AddTask} />
-          <Route path="/tasks/:id" component={EditTask} />
-        </Switch>
-      </div>
-=======
       <Navbar className="color-nav" variant="dark">
         <Navbar.Brand href="/"><b> Charing Cross Hospital Handover Portal</b></Navbar.Brand>
         <Nav className="mr-auto">
           <Nav.Link href="/"><b>Today's tasks</b></Nav.Link>
+          <Nav.Link href="/Archive"><b>Archive</b></Nav.Link>
           <Nav.Link href="/tasks/add"><b>Add a task</b></Nav.Link>
         </Nav>
       </Navbar>
       <Switch>
-        <Route exact path="/" component={TaskTable} />
-        <Route exact path="/tasks/add" component={AddTask} />
-        <Route path="/tasks/:id" component={EditTask} />
+          <Route exact path="/" component={TaskTable} />
+          <Route exact path="/Archive" component={Archive} />
+          <Route exact path="/tasks/add" component={AddTask} />
+          <Route path="/tasks/:id" component={EditTask} />
       </Switch>
->>>>>>> main
     </BrowserRouter>
   )
 }
