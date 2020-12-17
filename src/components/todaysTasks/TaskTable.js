f862a59e4090c3a3c5dafc2c79dee949fcdf1785
HelenaFP <helena.ferreira-pinto18@imperial.ac.uk>
2020-12-17 12:06:26 +0000

    - appearance change of header table - conditional change of color (green/red) when displaying Completed/Pending - change table order to the following: date, mrn, location, description, grade, status - change view/edit order to match table order

--- src/components/todaysTasks/TaskTable.js
+++ src/components/todaysTasks/TaskTable.js
@@ -23,14 +23,14 @@ export default function TaskTable() {
         <table className="table border shadow" >
           <thead className="thead-dark header-table">
             <tr>
-              <th scope="col" style={{color: 'SlateGrey'}}>#</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>DATE CREATED</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>DESCRIPTION</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>GRADE REQUIRED</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>MRN</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>LOCATION</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>STATUS</th>
-              <th scope="col" style={{color: 'SlateGrey'}}>MORE...</th>
+              <th scope="col" style={{color: 'White'}}>#</th>
+              <th scope="col" style={{color: 'White'}}>DATE CREATED</th>
+              <th scope="col" style={{color: 'White'}}>MRN</th>
+              <th scope="col" style={{color: 'White'}}>LOCATION</th>
+              <th scope="col" style={{color: 'White'}}>DESCRIPTION</th>
+              <th scope="col" style={{color: 'White'}}>GRADE REQUIRED</th>
+              <th scope="col" style={{color: 'White'}}>STATUS</th>
+              <th scope="col" style={{color: 'White'}}>MORE...</th>
             </tr>
           </thead>
           <tbody>
@@ -38,11 +38,16 @@ export default function TaskTable() {
               <tr key={task.id}>
                 <th scope="row">{index + 1}</th>
                 <td>{moment(task.dateCreated).format('LLL')}</td>
-                <td>{task.description}</td>
-                <td>{task.gradeRequired}</td>
                 <td>{task.patientMrn}</td>
                 <td>{task.patientLocation}</td>
-                <td>{task.completed ? "Completed" : "Pending..."}</td>
+                <td>{task.description}</td>
+                <td>{task.gradeRequired}</td>
+                {!task.completer && (
+                    <td style={{backgroundColor: '#e17055'}}>Pending...</td>
+                )}
+                {task.completer && (
+                    <td style={{backgroundColor: '#55efc4'}}>Completed</td>
+                )}
                 <td>
                   <Link className="btn btn-info mr-2" to={{pathname: `/tasks/${task.id}`, selectedTask: task}}>View/Edit</Link>
                 </td>
